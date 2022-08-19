2ac432a9a8c2f9608273df5bf2f0079c6f9ab833
缘 <111369757+b8l8u8e8@users.noreply.github.com>
2022-08-19 15:22:48 +0800

    Add files via upload

---
+++ 二维数组遍历.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main(){
+	int a[][4]={
+		{1,2,3,4},
+		{5,6,7,8},
+	};
+	int i,j;
+
+	for(i=0;i<2;i++){
+		if(i==1) printf("\n");
+		for(j=0;j<4;j++){
+		printf("%d ",a[i][j]);
+		}
+	}
+	
+	return 0;
+	
+}
