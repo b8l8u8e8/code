909fbe2fb6e3dffe130060bac445db6f69d23628
缘 <111369757+b8l8u8e8@users.noreply.github.com>
2022-08-19 13:27:15 +0800

    Add files via upload

---
+++ x-x2求和.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int sum();
+
+
+int main(){
+	int begin,end;
+	scanf("%d %d",&begin,&end);
+	printf("%d",sum(begin,end));
+	return 0;
+	
+}
+
+
+
+int sum(int begin,int end){
+	int i,x=0;
+	
+	for(i=begin;i<=end;i++){
+		x+=i;
+		
+	}
+	
+	
+	return x;
+}
