eca49a519c08eb22d75598112f03cc18bd71acc5
缘 <111369757+b8l8u8e8@users.noreply.github.com>
2022-08-22 21:33:23 +0800

    Add files via upload

--- 指针遍历数组.c
+++ 指针遍历数组.c
@@ -1,13 +1,14 @@
 #include<stdio.h>
 
 int main(){
-	char a[]={1,2,3,4,5,-1};
+	char a[]={1,2,3,4,5};
 	char *p=a;
-	while(*p!=-1){
+	//让p的地址小于等于a数组数值最后一个地址的条件即可完成遍历
+	while(p<=&a[sizeof(a)/sizeof(a[0])-1]){
 		printf("%d\n",*p++);
 	}
 	
-	
+
 	
 	return 0;
 	
