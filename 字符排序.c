eaa72fb1414b5c248342292279c85ac080f4c77e
缘 <111369757+b8l8u8e8@users.noreply.github.com>
2022-08-23 23:32:16 +0800

    Add files via upload

---
+++ 字符排序.c
@@ -0,0 +1,21 @@
+
+
+void zfpx(char a[],int len)
+{
+	int k=0,i=0,i2;
+	for(i2=len-1;i2>0;i2--){
+			 for(i=1;i<i2+1;i++){
+	     	if(a[k]<a[i]){
+			k=i;
+			//printf("%d",i);
+	    	}
+	    }
+	    int t;
+	    t=a[k];
+	    a[k]=a[i2];
+	    a[i2]=t;
+	//swap(a[k],a[i2]);
+		//printf("%c %c",a[k],a[i2]);
+   }
+    
+}
