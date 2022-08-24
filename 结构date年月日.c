666341c160d4d3745a503e3f06569047ba808e42
缘 <111369757+b8l8u8e8@users.noreply.github.com>
2022-08-24 23:16:06 +0800

    Add files via upload

--- 结构date年月日.c
+++ 结构date年月日.c
@@ -1,14 +1,15 @@
 #include<stdio.h>
 struct date{
 	int year,month,day;
-};
-struct date today;
+}today={2022,8,24};
+//struct date today;
 
 
 int main(){
-	today.year=2022;
-	today.month=8;
-	today.day=24;
+	//today.year=2022;
+	//today.month=8;
+	//today.day=24;
+	//struct date today={2022,8,24};
 	printf("现在是%d年%d月%d日",today.year,today.month,today.day);
 	
 	return 0;
