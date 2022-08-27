d991759e82d81185f9c547d0028cceb7adfcd0a3
缘 <111369757+b8l8u8e8@users.noreply.github.com>
2022-08-27 22:39:10 +0800

    Add files via upload

--- 宏定义.c
+++ 宏定义.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 #define pi 3.14
-#define prt  printf("%f",pi)
+#define prt  printf("%f\n",pi);\
+             printf("%s","多行也可以")
 
 //预先定义的宏(单纯文本替换) __FILE__ __TIME__  __LINE__ __FUNC__ __TIME__
 
