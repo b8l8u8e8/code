ddf9ffb177d5e0204a2fbd8cdbb47b13134ecefd
缘 <111369757+b8l8u8e8@users.noreply.github.com>
2022-08-28 21:44:28 +0800

    Add files via upload

--- 宏定义.c
+++ 宏定义.c
@@ -1,13 +1,14 @@
 #include<stdio.h>
 #define pi 3.14
 #define prt  printf("%f\n",pi);\
-             printf("%s","多行也可以")
-
+             printf("%s","多行也可以\n")
+//带参数宏没有类型
+#define compute(x) x*5
 //预先定义的宏(单纯文本替换) __FILE__ __TIME__  __LINE__ __FUNC__ __TIME__
 
 int main(){
 	prt;
-	
+	printf("%d",compute(3));
 	
 	return 0;
 }
