ab72eea7b1815d75ff2aeee7ac22328bb21cfdee
缘 <111369757+b8l8u8e8@users.noreply.github.com>
2022-08-29 22:46:14 +0800

    Add files via upload

---
+++ 统计数字字符个数.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<string.h>
+
+int main(){
+	char a[255];
+	int b=0;
+	gets(&a);
+//	scanf("%s",a);
+	//printf("%d",strlen(a));
+	for(int i=0;i<strlen(a);i++){
+		if(a[i]>='0'&&a[i]<='9'){
+		b++;
+		}
+	}
+	printf("%d",b);
+	return 0;
+	
+}
+/*
+  https://vjudge.csgrandeur.cn/contest/512622#problem/A
+ */
